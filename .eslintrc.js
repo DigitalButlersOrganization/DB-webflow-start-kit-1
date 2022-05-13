c3667678b590a52a913f00c5c1c7fb31b7035bdc
kirillsakun <kirillsakun03@gmail.com>
2022-05-13 13:54:57 +0400

    update (eslint): config update

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,21 @@ module.exports = {
 	parserOptions: {
 		ecmaVersion: 'latest',
 	},
+	plugins: [
+		'unicorn',
+	],
 	rules: {
 		'linebreak-style': 0,
 		'no-multiple-empty-lines': [1, { max: 2 }],
 		indent: [1, 'tab'],
 		'no-tabs': ['error', { allowIndentationTabs: true }],
+		'unicorn/prefer-add-event-listener': [
+			'error', {
+				excludedPackages: [
+					'koa',
+					'sax',
+				],
+			},
+		],
 	},
 };
